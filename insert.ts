532d437ef27bc6e621805d4af442eda6cacefca8
ch-shubham <shubham128166@gmail.com>
2020-05-26 22:16:16 +0530

    Added insert Function

---
+++ insert.ts
@@ -0,0 +1,17 @@
+/*
+** Works on List/Array
+** Adds the given element to the array at given index
+** Return the new copy of array without affecting original one.
+*/
+import curryN from "./utils/curry_n.ts"
+import { Curry3 } from "./utils/types.ts"
+
+function insert(index: number, element: any, list: Array<any>){
+    index = index < list.length && index >= 0 ? index : list.length
+    let result = [...list]
+    result.splice(index,0,element)
+    return result
+}
+
+
+export default <Curry3<number, any, Array<any>, Array<number>>>curryN(3, insert)
