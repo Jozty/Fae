bb7abcdb5f0c05a5576e6a993c4fc60509dee975
Shivam Singla <shivam.cse17@nituk.ac.in>
2020-05-30 03:49:31 +0530

    add tests for insert, reduce, sum

--- insert.ts
+++ insert.ts
@@ -1,16 +1,16 @@
+import curryN from "./utils/curry_n.ts"
+import { Curry3 } from "./utils/types.ts"
+
 /**
  * Returns a new array with `element` inserted at `index` to `list`
  * without affecting original one.
 */
-import curryN from "./utils/curry_n.ts"
-import { Curry3 } from "./utils/types.ts"
-
-function insert(index: number, element: any, list: ArrayLike<any>){
-    index = index < list.length && index >= 0 ? index : list.length
-    let result = Array.from(list)
-    result.splice(index, 0, element)
-    return result
+function insert(index: number, element: any, list: any[]) {
+  index = index < list.length && index >= 0 ? index : list.length
+  let result = Array.from(list)
+  result.splice(index, 0, element)
+  return result
 }
 
 
-export default <Curry3<number, any, Array<any>, Array<number>>>curryN(3, insert)
+export default curryN(3, insert) as Curry3<number, any, any[], any[]>
