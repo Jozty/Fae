e2bcbe708cf8bb26c8b07d7e8176750ef91166c8
Shivam Singla <shivam.cse17@nituk.ac.in>
2020-06-03 02:20:57 +0530

    attemp fix doc

--- trim.ts
+++ trim.ts
@@ -13,7 +13,7 @@ function _trim(str: string, t: string = '') {
 
 /** Trims the string `str` from both end with `t`.
  * Trims with white space if `t` is [''], with `t` otherwise.
- * @func
+ * @param {string} str
  * 
  *      Fae.trim('   xyz  ', ''); // 'xyz
  *      Fae.trim('[[Hello]]]', '[') // Hello]]]
