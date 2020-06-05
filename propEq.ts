dcad5d7c72244e238d069a0726e906afad035db7
ch-shubham <shubham128166@gmail.com>
2020-06-05 07:57:33 +0530

    adding propEq and removing comments from not.spec file

---
+++ propEq.ts
@@ -0,0 +1,19 @@
+import curryN from "./utils/curry_n.ts"
+import { Curry3, ObjRec } from "./utils/types.ts"
+import { equals } from './equals.ts'
+
+function _propEq(name: string, val: any, obj: ObjRec) {
+  return equals(val, obj[name])
+}
+
+/**
+ * Returns `true` if the specified object property is equal, to the given value; `false` otherwise.
+ *
+ *      const shivam = {name: 'shivam', age: 20, hair: 'brown'}
+ *      const shubham = {name: 'shubham', age: 22, hair: 'black'}
+ *      const Krish = {name: 'krish', age: 25, hair: 'black'}
+ *      const students = [abby, fred, rusty, alois]
+ *      const hasBrownHair = Fae.propEq('hair', 'brown')
+ *      Fae.filter(hasBrownHair, kids) //=> [shubham] 
+ */
+export const propEq: Curry3<string, any, ObjRec, boolean> = curryN(2, _propEq)
