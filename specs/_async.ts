f7470f04384ca08b4aaa34f9cce8853c3f246b90
Shivam Singla <shivam.cse17@nituk.ac.in>
2020-06-12 23:33:08 +0530

    add support for async tests

---
+++ specs/_async.ts
@@ -0,0 +1,16 @@
+
+export function it(fun: Function) {
+  return async function() {
+    let done: Function = () => void 0
+    const p = new Promise(resolve => {
+      let d = () => resolve()
+      done = d
+    })
+    await fun(done)
+    await p
+  }
+}
+
+export type Tests = {
+  [desc: string]: () => Promise<void>
+}
