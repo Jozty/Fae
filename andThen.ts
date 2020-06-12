e8b060766722f71cb7b68b93174eaecec8635fc3
Shivam Singla <shivam.cse17@nituk.ac.in>
2020-06-12 23:39:23 +0530

    add function - andThen
    https://github.com/Jozty/Fae/issues/26

--- andThen.ts
+++ andThen.ts
@@ -1,11 +1,15 @@
 import curryN from "./utils/curry_n.ts"
 import { Func, Curry2 } from "./utils/types.ts"
-import _assertPromise from "./utils/assertPromise.ts"
+import { assertPromise } from "./utils/assert.ts"
 
 function _andThen(f: Func, p: any) {
-  _assertPromise('andThen', p);
-
-  return p.then(f);
+  assertPromise('andThen', p)
+  return p.then(f)
 }
 
+/**
+ * Returns the result of applying the onSuccess function to the value inside
+ * a successfully resolved promise. This is useful for working with promises
+ * inside function compositions.
+ */
 export const andThen: Curry2<Func, any, Promise<any>> = curryN(2, _andThen)
--- utils/assert.ts
+++ utils/assert.ts
@@ -1,7 +1,10 @@
 import { isFunction } from './is.ts'
 
-export default function _assertPromise(name: any, p: any) {
+// TODO: (singla-shivam) change toString function
+ 
+export function assertPromise(name: string, p: any): p is PromiseLike<any> {
   if (p == null || !isFunction(p.then)) {
     throw new TypeError('`' + name + '` expected a Promise, received ' + p.toString())
   }
+  return true
 }
