0e5933eaa13ecd5d84860ef0b153e37a1a574bbd
Divakar Lakhera <divakarlakhera99@gmail.com>
2020-09-14 01:37:31 +0530

    Updated add.spec.ts
    Added new testcases.

--- specs/add.spec.ts
+++ specs/add.spec.ts
@@ -6,5 +6,9 @@ import { eq } from "./utils/utils.ts"
 describe('add', () => {
   it('should be declared correctly', () => {
     eq(add(10, 20), 30)
+    eq(add(-10,10),0)
+    eq(add(9999999,1),10000000)
+    eq(add(1999999,1),2000000)
+    eq(add(10,-1),9)
   })
 })
