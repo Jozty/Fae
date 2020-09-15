00b91d05a24030d286348e78c75ea2dc64a19ca5
shivam <shivam.cse17@nituk.ac.in>
2020-09-15 16:27:21 +0530

    test async functions

--- specs/andThen.test.ts
+++ specs/andThen.test.ts
@@ -1,5 +1,4 @@
-import { it, Tests } from './_async.ts'
-import { describe, it as itS} from "./_describe.ts"
+import { describe, it } from "./_describe.ts"
 import {
   andThen,
   compose,
@@ -8,61 +7,46 @@ import {
 } from '../mod.ts'
 import { eq, thr } from "./utils/utils.ts"
 
-const tests: Tests = {
-  "should invoke then on the promise with the function passed to it": it(
-    async (done: Function) => {
-      const p = new Promise(resolve => {
-        setTimeout(() => {
-          resolve(1)
-        }, 100)
-      })
-      andThen(
-        function (n) {
-          eq(n, 1)
-          done()
-        },
-        p
-      )
-    }
-  ),
-
-  "should flatten promise returning functions": it(
-    async (done: Function) => {
-      const incAndWrap = compose(Promise.resolve.bind(Promise), inc)
-      const asyncAddThree = pipe(incAndWrap, andThen(incAndWrap), andThen(incAndWrap))
-  
-      andThen((result) => {
-        eq(result, 4)
-        done()
-      })(asyncAddThree(1))
-    }
-  ),
-
-  "should not dependent on a particular promise implementation": it(
-    async (done: Function) => {
-      const thennable = {
-        then: function(f: Function) {
-          return f(42)
-        }
-      }
-  
-      const f = function(n: number) {
-        eq(n, 42)
-        done()
-      }
-  
-      andThen(f, thennable)
-    }
-  ),
-}
-
 describe('andThen', () => {
-  itS('throws a typeError if the then method does not exist', () => {
+  it('throws a typeError if the then method does not exist', () => {
     thr(() => andThen(inc, 1), '`andThen` expected a Promise, received 1')
 
   })
-})
 
-for(let desc in tests) {
-  Deno.test(desc, tests[desc])
-}
+  it('should invoke then on the promise with the function passed to it', async () => {
+    const p = new Promise(resolve => {
+      setTimeout(() => {
+        resolve(1)
+      }, 1000)
+    })
+    await andThen(
+      function (n) {
+        eq(n, 1)
+      },
+      p
+    )
+  })
+
+  it('should flatten promise returning functions', async () => {
+    const incAndWrap = compose(Promise.resolve.bind(Promise), inc)
+    const asyncAddThree = pipe(incAndWrap, andThen(incAndWrap), andThen(incAndWrap))
+
+    await andThen((result) => {
+      eq(result, 4)
+    })(asyncAddThree(1))
+  })
+
+  it('should not dependent on a particular promise implementation', async () => {
+    const thennable = {
+      then: function (f: Function) {
+        return f(42)
+      }
+    }
+
+    const f = function (n: number) {
+      eq(n, 42)
+    }
+
+    await andThen(f, thennable)
+  })
+})
