979f2939288ee1432428e428623b602b5127b8e2
Your NamemeeAndyou <yadavmeenu302@gmail.com>
2020-09-19 19:45:27 +0530

    added tests for add, multiply, subtract, divide

--- specs/add.test.ts
+++ specs/add.test.ts
@@ -1,9 +1,22 @@
 import { describe, it, expect } from './_describe.ts'
-import { add } from '../mod.ts'
+import { add, _ } from '../mod.ts'
 import { eq } from './utils/utils.ts'
 
 describe('add', () => {
   it('should be declared correctly', () => {
     eq(add(10, 20), 30)
+    eq(add(NaN, 10), NaN)
+    eq(add(Infinity, 4), Infinity)
+    eq(add(25, Infinity), Infinity)
+    eq(add(Infinity, Infinity), Infinity)
+    eq(add(0, 0), 0)
+    eq(add(25, 0), 25)
+  })
+
+  it('should test curried versions too', () => {
+    eq(add(25)(25), 50)
+    eq(add(_, 25)(25), 50)
+    eq(add(25, _)(25), 50)
+    eq(add(_, _)(25)(25), 50)
   })
 })
