50ed438ab8e4a45dc7d9d1060760aa37f511133b
shivam <shivam.cse17@nituk.ac.in>
2020-09-21 02:53:30 +0530

    format types

--- divide.ts
+++ divide.ts
@@ -2,14 +2,19 @@ import curryN from './utils/curry_n.ts'
 import type { PH } from './utils/types.ts'
 
 // @types
-type Divide_2 = ((b: number) => number) & ((b?: PH) => Divide_2)
+// prettier-ignore
+type Divide_2 = ((b: number) => number)
+  & ((b?: PH) => Divide_2)
 
-type Divide_1 = ((a: number) => number) & ((a?: PH) => Divide_1)
+// prettier-ignore
+type Divide_1 = ((a: number) => number)
+  & ((a?: PH) => Divide_1)
 
-type Divide = ((a: number, b: number) => number) &
-  ((a: number, b?: PH) => Divide_2) &
-  ((a: PH, b: number) => Divide_1) &
-  ((a?: PH, b?: PH) => Divide)
+// prettier-ignore
+type Divide = ((a: number, b: number) => number)
+  & ((a: number, b?: PH) => Divide_2)
+  & ((a: PH, b: number) => Divide_1)
+  & ((a?: PH, b?: PH) => Divide)
 
 function _divide(a: number, b: number) {
   return a / b
--- zipWith.ts
+++ zipWith.ts
@@ -2,69 +2,45 @@ import type { PH } from './utils/types.ts'
 import curryN from './utils/curry_n.ts'
 
 // @types
-type ZipWith_1<T1, T2> = (<R>(fn: (a: T1, b: T2) => R) => R[]) &
-  ((fn?: PH) => ZipWith_1<T1, T2>)
+// prettier-ignore
+type ZipWith_1<T1, T2> = (<R>(fn: (a: T1, b: T2) => R) => R[])
+  & ((fn?: PH) => ZipWith_1<T1, T2>)
 
-type ZipWith_2<T1, T2, R> = ((list1: T1[]) => R[]) &
-  ((list1?: PH) => ZipWith_2<T1, T2, R>)
+// prettier-ignore
+type ZipWith_2<T1, T2, R> = ((list1: T1[]) => R[])
+  & ((list1?: PH) => ZipWith_2<T1, T2, R>)
 
-type ZipWith_3<T1, T2, R> = ((list2: T2[]) => R[]) &
-  ((list2?: PH) => ZipWith_3<T1, T2, R>)
+// prettier-ignore
+type ZipWith_3<T1, T2, R> = ((list2: T2[]) => R[])
+  & ((list2?: PH) => ZipWith_3<T1, T2, R>)
 
-type ZipWith_2_3<T1, T2, R> = ((list1: T1[], list2: T2[]) => R[]) &
-  ((list1: T1[], list2?: PH) => ZipWith_3<T1, T2, R>) &
-  (<L extends ArrayLike<T> | string, T = any>(
-    list1: PH,
-    list2: T2[],
-  ) => ZipWith_2<T1, T2, R>) &
-  ((list1?: PH, list2?: PH) => ZipWith_2_3<T1, T2, R>)
+// prettier-ignore
+type ZipWith_2_3<T1, T2, R> = ((list1: T1[], list2: T2[]) => R[])
+  & ((list1: T1[], list2?: PH) => ZipWith_3<T1, T2, R>)
+  & (<L extends ArrayLike<T> | string, T = any>(list1: PH, list2: T2[]) => ZipWith_2<T1, T2, R>)
+  & ((list1?: PH, list2?: PH) => ZipWith_2_3<T1, T2, R>)
 
-type ZipWith_1_3<T1> = (<T2, R>(
-  fn: (a: T1, b: T2) => R,
-  list2: T2[],
-) => R[]) &
-  (<T2, R>(
-    fn: (a: T1, b: T2) => R,
-    list2?: PH,
-  ) => ZipWith_3<T1, T2, R>) &
-  (<T2>(fn: PH, list2: T2[]) => ZipWith_1<T1, T2>) &
-  ((fn?: PH, list2?: PH) => ZipWith_1_3<T1>)
+// prettier-ignore
+type ZipWith_1_3<T1> = (<T2, R>(fn: (a: T1, b: T2) => R, list2: T2[]) => R[])
+  & (<T2, R>(fn: (a: T1, b: T2) => R, list2?: PH) => ZipWith_3<T1, T2, R>)
+  & (<T2>(fn: PH, list2: T2[]) => ZipWith_1<T1, T2>)
+  & ((fn?: PH, list2?: PH) => ZipWith_1_3<T1>)
 
-type ZipWith_1_2<T2> = (<T1, R>(
-  fn: (a: T1, b: T2) => R,
-  list1: T1[],
-) => R[]) &
-  (<T1, R>(
-    fn: (a: T1, b: T2) => R,
-    list1?: PH,
-  ) => ZipWith_2<T1, T2, R>) &
-  (<T1>(fn: PH, list1: T1[]) => ZipWith_1<T1, T2>) &
-  ((fn?: PH, list1?: PH) => ZipWith_1_2<T2>)
+// prettier-ignore
+type ZipWith_1_2<T2> = (<T1, R>(fn: (a: T1, b: T2) => R, list1: T1[]) => R[])
+  & (<T1, R>(fn: (a: T1, b: T2) => R, list1?: PH) => ZipWith_2<T1, T2, R>)
+  & (<T1>(fn: PH, list1: T1[]) => ZipWith_1<T1, T2>)
+  & ((fn?: PH, list1?: PH) => ZipWith_1_2<T2>)
 
-type ZipWith = (<T1, T2, R>(
-  fn: (a: T1, b: T2) => R,
-  list1: T1[],
-  list2: T2[],
-) => R[]) &
-  ((fn?: PH, list1?: PH, list2?: PH) => ZipWith) &
-  (<T1, T2, R>(
-    fn: (a: T1, b: T2) => R,
-    list1?: PH,
-    list2?: PH,
-  ) => ZipWith_2_3<T1, T2, R>) &
-  (<T1>(fn: PH, list1: T1[], list2?: PH) => ZipWith_1_3<T1>) &
-  (<T2>(fn: PH, list1: PH, list2: T2[]) => ZipWith_1_2<T2>) &
-  (<T1, T2, R>(
-    fn: (a: T1, b: T2) => R,
-    list1: T1[],
-    list2?: PH,
-  ) => ZipWith_3<T1, T2, R>) &
-  (<T1, T2, R>(
-    fn: (a: T1, b: T2) => R,
-    list1: PH,
-    list2: T2[],
-  ) => ZipWith_2<T1, T2, R>) &
-  (<T1, T2>(fn: PH, list1: T1[], list2: T2[]) => ZipWith_1<T1, T2>)
+// prettier-ignore
+type ZipWith = (<T1, T2, R>(fn: (a: T1, b: T2) => R, list1: T1[], list2: T2[]) => R[])
+  & ((fn?: PH, list1?: PH, list2?: PH) => ZipWith)
+  & (<T1, T2, R>(fn: (a: T1, b: T2) => R, list1?: PH, list2?: PH) => ZipWith_2_3<T1, T2, R>)
+  & (<T1>(fn: PH, list1: T1[], list2?: PH) => ZipWith_1_3<T1>)
+  & (<T2>(fn: PH, list1: PH, list2: T2[]) => ZipWith_1_2<T2>)
+  & (<T1, T2, R>(fn: (a: T1, b: T2) => R, list1: T1[], list2?: PH) => ZipWith_3<T1, T2, R>)
+  & (<T1, T2, R>(fn: (a: T1, b: T2) => R, list1: PH, list2: T2[]) => ZipWith_2<T1, T2, R>)
+  & (<T1, T2>(fn: PH, list1: T1[], list2: T2[]) => ZipWith_1<T1, T2>)
 
 function _zipWith<T1, T2, R>(
   fn: (a: T1, b: T2) => R,
