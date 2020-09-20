5d8d4cd8906073e73acb960c340b872d2c3a36fe
Meenu Yadav <yadavmeenu302@gmail.com>
2020-09-20 19:07:57 +0530

    Add tests in zip and zipWith

--- specs/zip.test.ts
+++ specs/zip.test.ts
@@ -1,32 +1,51 @@
 import { describe, it } from './_describe.ts'
-import { zip } from '../mod.ts'
+import { zip, map, filter } from '../mod.ts'
 import { eq } from './utils/utils.ts'
 
 describe('zip', () => {
   it('should return an array of "tuples"', () => {
     const a = [1, 2, 3]
     const b = [100, 200, 300]
+    function multiply3(x: number) {
+      return 3 + x
+    }
 
     eq(zip(a, b), [
       [1, 100],
       [2, 200],
-      [3, 300],
+      [3, 300]
+    ])
+    
+    eq(zip(map(multiply3)(a) as number[], b), [
+      [3, 100],
+      [6, 200],
+      [9, 300]
     ])
   })
 
-  it('should return a list as long as the shorter of the lists input', () => {
-    const a = [1, 2, 3]
+  it('should return a list truncated to the length of shorter of the two input lists', () => {
+    const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const b = [100, 200, 300, 400]
     const c = [10, 20]
+    const d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    function even(x: number) {
+      return x % 2 === 0
+    }
 
     eq(zip(a, b), [
       [1, 100],
       [2, 200],
-      [3, 300],
+      [3, 300]
     ])
-    eq(zip(a, c), [
-      [1, 10],
-      [2, 20],
+    eq(zip(b, c), [
+      [100, 10],
+      [200, 20]
+    ])
+    eq(zip(filter(even)(a) as number[], b), [
+      [2, 100],
+      [4, 200],
+      [6, 300],
+      [8, 400]
     ])
   })
 })
--- specs/zipWith.test.ts
+++ specs/zipWith.test.ts
@@ -1,13 +1,17 @@
-import { describe, it } from './_describe.ts'
-import { zipWith, add, multiply, _ } from '../mod.ts'
-import { eq } from './utils/utils.ts'
+import { describe, it } from "./_describe.ts";
+import { zipWith, add, multiply, _ } from "../mod.ts";
+import { eq } from "./utils/utils.ts";
+import { filter } from "../filter.ts";
 
-describe('zipWith', () => {
+describe("zipWith", () => {
   const a = [1, 2, 3]
   const b = [100, 200, 300]
   const c = [10, 20, 30, 40, 50, 60]
-  const s = (a: any, b: any) => a + ' cow ' + b
-  it('should return an array created by applying its passed-in function pair-wise on its passed in arrays', () => {
+  const s = (a: any, b: any) => a + " cow " + b
+  const y = ["Cow", "Horse", "Dog"]
+  const f = zipWith((a: any, b: String) => a + " " + b)
+
+  it("should return an array created by applying its passed-in function pair-wise on its passed in arrays", () => {
     const z = zipWith(_, a, b)
     eq(z(add as (a: number, b: number) => number), [101, 202, 303])
     eq(z(multiply as (a: number, b: number) => number), [
@@ -15,13 +19,15 @@ describe('zipWith', () => {
       400,
       900,
     ])
-    eq(z(s), ['1 cow 100', '2 cow 200', '3 cow 300'])
+    eq(z(s), ["1 cow 100", "2 cow 200", "3 cow 300"])
+    eq(f(a)(y), ["1 Cow", "2 Horse", "3 Dog"])
   })
 
-  it('should return an array whose length is equal to the shorter of its input arrays', () => {
+  it("should return an array whose length is equal to the shorter of its input arrays", () => {
     eq(
       zipWith(add as (a: number, b: number) => number, a, c).length,
       a.length,
     )
-  })
+    eq(f(c)(y), ["10 Cow", "20 Horse", "30 Dog"])
+  }) 
 })
