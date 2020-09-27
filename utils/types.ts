deffdeeb7708abee30f784ff29b064438fd27488
shivam <shivam.cse17@nituk.ac.in>
2020-09-28 00:09:16 +0530

    remove redundant signatures in types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -12,8 +12,6 @@ export interface Curry1<T, R = T> {
 
 /** Function type for curried function of arity 2 */
 export interface Curry2<T1, T2 = T1, R = T1> {
-  (t1?: PlaceHolder): Curry2<T1, T2, R>
-  (t1: T1): Curry1<T2, R>
   (t1?: PlaceHolder, t2?: PlaceHolder): Curry2<T1, T2, R>
   (t1: T1, t2?: PlaceHolder): Curry1<T2, R>
   (t1: PlaceHolder, t2: T2): Curry1<T1, R>
@@ -22,12 +20,6 @@ export interface Curry2<T1, T2 = T1, R = T1> {
 
 /** Function type for curried function of arity 3 */
 export interface Curry3<T1, T2 = T1, T3 = T1, R = T1> {
-  (t1?: PlaceHolder): Curry3<T1, T2, T3, R>
-  (t1: T1): Curry2<T2, T3, R>
-  (t1?: PlaceHolder, t2?: PlaceHolder): Curry3<T1, T2, T3, R>
-  (t1: T1, t2?: PlaceHolder): Curry2<T2, T3, R>
-  (t1: PlaceHolder, t2: T2): Curry2<T1, T3, R>
-  (t1: T1, t2: T2): Curry1<T3, R>
   (t1?: PlaceHolder, t2?: PlaceHolder, t3?: PlaceHolder): Curry3<
     T1,
     T2,
