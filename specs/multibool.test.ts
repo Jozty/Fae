65fe3efedbc9a101c5b9ff98401b2cceb5e6026d
Divakar Lakhera <divakarlakhera99@gmail.com>
2020-09-27 23:30:03 +0530

    add comments

--- specs/multibool.test.ts
+++ specs/multibool.test.ts
@@ -6,20 +6,20 @@ import { xor } from '../mod.ts'
 
 describe('mutlibool', () => {
   it(' Should solve (a + b) . (c + d. e + (f + g)) ', () => {
-    const orOpr1 = or([], 1)
-    const orOpr2 = or({ 1: 3 }, 'A')
-    const andOp1 = and(false, 4)
-    const orMulti = or(or(orOpr2, andOp1), NaN)
-    const total = and(orOpr1, orMulti)
+    const orOpr1 = or([], 1) // (a+b)
+    const orOpr2 = or({ 1: 3 }, 'A') // (f+g)
+    const andOp1 = and(false, 4) // (d.e)
+    const orMulti = or(or(orOpr2, andOp1), NaN) // (c+d.e+(f+g))
+    const total = and(orOpr1, orMulti) // (a+b).(c+d.e+(f+g))
     eq(total, true)
   })
 
   it(' Should solve (a + b) ^ (c + d ^ e + (f + g)) ', () => {
-    const orOpr1 = or([], 1)
-    const orOpr2 = or({ 1: 3 }, 'A')
-    const xorOp1 = xor(false, 4)
-    const orMulti = or(or(orOpr2, xorOp1), undefined)
-    const total = xor(orOpr1, orMulti)
+    const orOpr1 = or([], 1) // (a+b)
+    const orOpr2 = or({ 1: 3 }, 'A') // (f+g)
+    const xorOp1 = xor(false, 4) // (d^e)
+    const orMulti = or(or(orOpr2, xorOp1), undefined) // (c+d^e+(f+g))
+    const total = xor(orOpr1, orMulti) // (a+b)^(c+d^e+(f+g))
     eq(total, false)
   })
 })
