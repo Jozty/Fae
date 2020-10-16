4499a57bf620a63826a290fd0ef565cf273d82e2
shivam <shivam.cse17@nituk.ac.in>
2020-10-16 22:34:12 +0530

    Add types and test for functions
    1. Props
    2. PropsStatisfies

--- props.ts
+++ props.ts
@@ -1,28 +1,20 @@
 import type { Obj, PH } from './utils/types.ts'
 import { prop } from './prop.ts'
+import type { Prop } from './prop.ts'
 import curryN from './utils/curry_n.ts'
 
 // @types
-type Props_2 = (<T>(
-  obj: Obj<T> | ArrayLike<T>,
-) => (T | undefined)[]) &
-  ((obj?: PH) => Props_2)
+type Props_2 = <T>(obj: Obj<T> | ArrayLike<T>) => (T | undefined)[]
 
-type Props_1<T> = ((p: (string | number)[]) => (T | undefined)[]) &
-  ((p?: PH) => Props_1<T>)
+type Props_1<T> = (p: Prop[]) => (T | undefined)[]
 
-type Props = (<T>(
-  p: (string | number)[],
-  obj: Obj<T> | ArrayLike<T>,
-) => (T | undefined)[]) &
-  ((p: (string | number)[], obj?: PH) => Props_2) &
-  (<T>(p: PH, obj: Obj<T> | ArrayLike<T>) => Props_1<T>) &
-  ((p?: PH, obj?: PH) => Props)
+// prettier-ignore
+type Props =
+  & ((p: Prop[], obj?: PH) => Props_2)
+  & (<T>(p: PH, obj: Obj<T> | ArrayLike<T>) => Props_1<T>)
+  & (<T>(p: Prop[], obj: Obj<T> | ArrayLike<T>,) => (T | undefined)[])
 
-function _props<T>(
-  p: (string | number)[],
-  obj: Obj<T> | ArrayLike<T>,
-) {
+function _props<T>(p: Prop[], obj: Obj<T> | ArrayLike<T>) {
   return p.map((a) => prop<T>(a, obj))
 }
 
