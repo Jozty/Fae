4f54febf371b8ef6baafa451e9b835211bfa9a07
MASTERDAMMU <64116979+MASTERDAMMU@users.noreply.github.com>
2020-10-18 11:41:53 +0530

    correct format
    updated format

--- concat.ts
+++ concat.ts
@@ -3,23 +3,22 @@ import type { PH } from './utils/types.ts'
 import { isArray, isString } from './utils/is.ts'
 
 // @types
-type Concat_2<L extends T[] | string, T> = ((b: L) => L) &
-  ((b?: PH) => Concat_2<L, T>)
+type Concat_2<L extends T[] | string, T> = (b: L) => L
+  
+type Concat_1<L extends T[] | string, T> = (a: L) => L
 
-type Concat_1<L extends T[] | string, T> = ((a: L) => L) &
-  ((a?: PH) => Concat_1<L, T>)
-
-type Concat = (<L extends T[] | string, T>(a: L, b: L) => L) &
-  (<L extends T[] | string, T>(a: L, b?: PH) => Concat_2<L, T>) &
-  (<L extends T[] | string, T>(a: PH, b: L) => Concat_1<L, T>) &
-  ((a?: PH, b?: PH) => Concat)
+// prettier-ignore
+type Concat =
+  &(<L extends T[] | string, T>(a: L, b?: PH) => Concat_2<L, T>)
+  &(<L extends T[] | string, T>(a: PH, b: L) => Concat_1<L, T>)
+  &(<L extends T[] | string, T>(a: L, b: L) => L)
 
 function _concat<L extends T[] | string, T>(a: L, b: L): L {
-  if (isArray<T>(a) && isArray<T>(b)) return a.concat(b) as L
-  if (isString(a) && isString(b)) return (a + b) as L
-  throw new TypeError(
-    'Types are not compatible. Both the arguments passed must be of same type.',
-  )
+  if (isArray<T>(a) && isArray<T>(b)) 
+    return a.concat(b) as L
+  if (isString(a) && isString(b)) 
+    return (a + b) as L
+  throw new TypeError('Types are not compatible. Both the arguments passed must be of same type.', )
 }
 
 /**
