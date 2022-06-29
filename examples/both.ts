94dd2e080a25727532ae10e86ad3bb92c21eb9aa
Neel Ratn <neel.ece17@nituk.ac.in>
2022-06-30 01:15:45 +0530

    Feature: Add examples for logical functions (#65)
    * feat: updated ts files for and, or & not
    
    * bug-fix: updated examples.ts
    
    * feat: added examples for logical functions
    
    * updated emppty line
    
    * feat: updated example.ts
    
    * feat: updated logical functions with examples

---
+++ examples/both.ts
@@ -0,0 +1,16 @@
+import * as Fae from 'https://deno.land/x/fae/mod.ts'
+
+const isGreaterThan5 = (x: number) => x > 5
+const isLessThan13 = (x: number) => x < 13
+const isOdd = (y: number) => y % 2 !== 0
+const isLessThan1 = (x: number) => x < 1
+
+const isBetween5And13 = Fae.both(isGreaterThan5, isLessThan13)
+const oddLessThan1 = Fae.both(isLessThan1, isOdd)
+const oddGreaterThan5 = Fae.both(isOdd, isGreaterThan5)
+
+console.assert(isBetween5And13(12) === true)
+console.assert(oddLessThan1(3) === false)
+console.assert(oddGreaterThan5(21) === true)
+
+console.log('Example ran successfully')
---
+++ examples/not.ts
@@ -0,0 +1,16 @@
+import * as Fae from 'https://deno.land/x/fae/mod.ts'
+
+// "not" with single input arguments
+// returns false args is true else false
+console.assert(Fae.not('') === true)
+console.assert(Fae.not(false) === true)
+console.assert(Fae.not(undefined) === true)
+console.assert(Fae.not(null) === true)
+
+console.assert(Fae.not(2) === false)
+console.assert(Fae.not(true) === false)
+console.assert(Fae.not('ab') === false)
+console.assert(Fae.not([1, 2]) === false)
+console.assert(Fae.not({ a: 1, b: 3 }) === false)
+
+console.log('Example ran successfully')
