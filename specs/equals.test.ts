73da90cb80d40e842c44c8e0455e02a168f1c6ca
Jyoti Jangid <jyoti.jangid8126@gmail.com>
2022-07-06 18:56:52 +0000

    resolved formatting errors

--- specs/equals.test.ts
+++ specs/equals.test.ts
@@ -1,87 +1,87 @@
-import { describe, it } from "./_describe.ts";
-import { _, equals } from "../mod.ts";
-import { eq } from "./utils/utils.ts";
+import { describe, it } from './_describe.ts';
+import { _, equals } from '../mod.ts';
+import { eq } from './utils/utils.ts';
 
-describe("equals", () => {
+describe('equals', () => {
   const a: any[] = [];
   const b = a;
-  it("should test for deep equality of its operands", () => {
+  it('should test for deep equality of its operands', () => {
     eq(equals(100, 100), true);
-    eq(equals(100, "100"), false);
+    eq(equals(100, '100'), false);
     eq(equals([], []), true);
     eq(equals(a, b), true);
   });
 
-  it("should consider equal Boolean primitives equal", () => {
+  it('should consider equal Boolean primitives equal', () => {
     eq(equals(true, true), true);
     eq(equals(false, false), true);
     eq(equals(true, false), false);
     eq(equals(false, true), false);
   });
 
-  it("should consider equivalent Boolean objects equal", () => {
+  it('should consider equivalent Boolean objects equal', () => {
     eq(equals(new Boolean(true), new Boolean(true)), true);
     eq(equals(new Boolean(false), new Boolean(false)), true);
     eq(equals(new Boolean(true), new Boolean(false)), false);
     eq(equals(new Boolean(false), new Boolean(true)), false);
   });
 
-  it("should never consider Boolean primitive equal to Boolean object", () => {
+  it('should never consider Boolean primitive equal to Boolean object', () => {
     eq(equals(true, new Boolean(true)), false);
     eq(equals(new Boolean(true), true), false);
     eq(equals(false, new Boolean(false)), false);
     eq(equals(new Boolean(false), false), false);
   });
 
-  it("should consider equal number primitives equal", () => {
+  it('should consider equal number primitives equal', () => {
     eq(equals(0, 0), true);
     eq(equals(0, 1), false);
     eq(equals(1, 0), false);
   });
 
-  it("should consider equivalent Number objects equal", () => {
+  it('should consider equivalent Number objects equal', () => {
     eq(equals(new Number(0), new Number(0)), true);
     eq(equals(new Number(0), new Number(1)), false);
     eq(equals(new Number(1), new Number(0)), false);
   });
 
-  it("should never consider number primitive equal to Number object", () => {
+  it('should never consider number primitive equal to Number object', () => {
     eq(equals(0, new Number(0)), false);
     eq(equals(new Number(0), 0), false);
   });
 
-  it("should consider equal string primitives equal", () => {
-    eq(equals("", ""), true);
-    eq(equals("", "x"), false);
-    eq(equals("x", ""), false);
-    eq(equals("foo", "foo"), true);
-    eq(equals("foo", "bar"), false);
-    eq(equals("bar", "foo"), false);
+  it('should consider equal string primitives equal', () => {
+    eq(equals('', ''), true);
+    eq(equals('', 'x'), false);
+    eq(equals('x', ''), false);
+    eq(equals('foo', 'foo'), true);
+    eq(equals('foo', 'bar'), false);
+    eq(equals('bar', 'foo'), false);
   });
 
-  it("should return the curried version", () => {
-    eq(equals(_, "")(""), true);
-    eq(equals("")("x"), false);
-    eq(equals("x")(""), false);
+  it('should return the curried version', () => {
+    eq(equals(_, '')(''), true);
+    eq(equals('')('x'), false);
+    eq(equals('x')(''), false);
   });
 
-  it("should consider equivalent String objects equal", () => {
-    eq(equals(new String(""), new String("")), true);
-    eq(equals(new String(""), new String("x")), false);
-    eq(equals(new String("x"), new String("")), false);
-    eq(equals(new String("foo"), new String("foo")), true);
-    eq(equals(new String("foo"), new String("bar")), false);
-    eq(equals(new String("bar"), new String("foo")), false);
+  it('should consider equivalent String objects equal', () => {
+    eq(equals(new String(''), new String('')), true);
+    eq(equals(new String(''), new String('x')), false);
+    eq(equals(new String('x'), new String('')), false);
+    eq(equals(new String('foo'), new String('foo')), true);
+    eq(equals(new String('foo'), new String('bar')), false);
+    eq(equals(new String('bar'), new String('foo')), false);
   });
 
-  it("should never consider string primitive equal to String object", () => {
-    eq(equals("", new String("")), false);
-    eq(equals(new String(""), ""), false);
-    eq(equals("x", new String("x")), false);
-    eq(equals(new String("x"), "x"), false);
+  it('should never consider string primitive equal to String object', () => {
+    eq(equals('', new String('')), false);
+    eq(equals(new String(''), ''), false);
+    eq(equals('x', new String('x')), false);
+    eq(equals(new String('x'), 'x'), false);
   });
 
-  it("should handle objects", () => {
+  it('should handle objects', () => {
     eq(equals({}, {}), true);
     eq(equals({ a: 1, b: 2 }, { a: 1, b: 2 }), true);
     eq(equals({ a: 2, b: 3 }, { b: 3, a: 2 }), true);
@@ -89,7 +89,7 @@ describe("equals", () => {
     eq(equals({ a: 2, b: 3, c: 1 }, { a: 2, b: 3 }), false);
   });
 
-  it("should considers equivalent Arguments objects equal", () => {
+  it('should considers equivalent Arguments objects equal', () => {
     const a = (function () {
       return arguments;
     })();
@@ -115,16 +115,16 @@ describe("equals", () => {
     eq(equals(c, a), false);
   });
 
-  it("should consider equivalent Error objects equal", () => {
-    eq(equals(new Error("XXX"), new Error("XXX")), true);
-    eq(equals(new Error("XXX"), new Error("YYY")), false);
-    eq(equals(new Error("XXX"), new TypeError("XXX")), false);
-    eq(equals(new Error("XXX"), new TypeError("YYY")), false);
+  it('should consider equivalent Error objects equal', () => {
+    eq(equals(new Error('XXX'), new Error('XXX')), true);
+    eq(equals(new Error('XXX'), new Error('YYY')), false);
+    eq(equals(new Error('XXX'), new TypeError('XXX')), false);
+    eq(equals(new Error('XXX'), new TypeError('YYY')), false);
   });
 
   let supportsSticky = false;
   try {
-    RegExp("", "y");
+    RegExp('', 'y');
     supportsSticky = true;
   } catch (e) {
     // does not support stickyflag
@@ -132,13 +132,13 @@ describe("equals", () => {
 
   let supportsUnicode = false;
   try {
-    RegExp("", "u");
+    RegExp('', 'u');
     supportsUnicode = true;
   } catch (e) {
     // does not support Unicode
   }
 
-  it("should handle regex", () => {
+  it('should handle regex', () => {
     eq(equals(/\s/, /\s/), true);
     eq(equals(/\s/, /\d/), false);
     eq(equals(/a/gi, /a/gi), true);
@@ -159,7 +159,7 @@ describe("equals", () => {
   const listA = [1, 2, 3];
   const listB = [1, 3, 2];
 
-  it("should handle lists", () => {
+  it('should handle lists', () => {
     eq(equals([], {}), false);
     eq(equals(listA, listB), false);
     eq(equals([1, 2, 3], [1, 2, 3]), true);
@@ -179,21 +179,21 @@ describe("equals", () => {
   const nestB = { a: [1, 2, { c: 1 }], b: 1 };
   const nestC = { a: [1, 2, { c: 2 }], b: 1 };
 
-  it("should handle recursive data structures", () => {
+  it('should handle recursive data structures', () => {
     eq(equals(c, d), true);
     eq(equals(e, f), true);
     eq(equals(nestA, nestB), true);
     eq(equals(nestA, nestC), false);
   });
 
-  it("should handle dates", () => {
+  it('should handle dates', () => {
     eq(equals(new Date(0), new Date(0)), true);
     eq(equals(new Date(1), new Date(1)), true);
     eq(equals(new Date(0), new Date(1)), false);
     eq(equals(new Date(1), new Date(0)), false);
   });
 
-  it("should require that both objects have the same enumerable properties with the same values", () => {
+  it('should require that both objects have the same enumerable properties with the same values', () => {
     const a1: any = [];
     const a2: any = [];
     a2.x = 0;
@@ -214,8 +214,8 @@ describe("equals", () => {
     const r2: any = /(?:)/;
     r2.x = 0;
 
-    const s1: any = new String("");
-    const s2: any = new String("");
+    const s1: any = new String('');
+    const s2: any = new String('');
     s2.x = 0;
 
     eq(equals(a1, a2), false);
@@ -240,43 +240,43 @@ describe("equals", () => {
   // @ts-ignore
   typArr3[0] = 0;
 
-  it("should handle typed arrays", () => {
+  it('should handle typed arrays', () => {
     eq(equals(typArr1, typArr2), true);
     eq(equals(typArr1, typArr3), false);
     eq(equals(typArr1, intTypArr), false);
   });
 
-  it("should compare Promise objects by identity", () => {
+  it('should compare Promise objects by identity', () => {
     const p = Promise.resolve(42);
     const q = Promise.resolve(42);
     eq(equals(p, p), true);
     eq(equals(p, q), false);
   });
 
-  it("should compare Map objects by value", () => {
+  it('should compare Map objects by value', () => {
     eq(equals(new Map([]), new Map([])), true);
-    eq(equals(new Map([]), new Map([[1, "a"]])), false);
-    eq(equals(new Map([[1, "a"]]), new Map([])), false);
-    eq(equals(new Map([[1, "a"]]), new Map([[1, "a"]])), true);
+    eq(equals(new Map([]), new Map([[1, 'a']])), false);
+    eq(equals(new Map([[1, 'a']]), new Map([])), false);
+    eq(equals(new Map([[1, 'a']]), new Map([[1, 'a']])), true);
     // prettier-ignore
     eq(
-      equals(new Map([[1, "a"], [2, "b"]]), new Map([[2, "b"], [1, "a"]])),
+      equals(new Map([[1, 'a'], [2, 'b']]), new Map([[2, 'b'], [1, 'a']])),
       true,
     );
-    eq(equals(new Map([[1, "a"]]), new Map([[2, "a"]])), false);
-    eq(equals(new Map([[1, "a"]]), new Map([[1, "b"]])), false);
+    eq(equals(new Map([[1, 'a']]), new Map([[2, 'a']])), false);
+    eq(equals(new Map([[1, 'a']]), new Map([[1, 'b']])), false);
 
-    const m1 = new Map<number, string | Map<number, string>>([[1, "a"], [
+    const m1 = new Map<number, string | Map<number, string>>([[1, 'a'], [
       2,
-      new Map([[3, "c"]]),
+      new Map([[3, 'c']]),
     ]]);
-    const m2 = new Map<number, string | Map<number, string>>([[1, "a"], [
+    const m2 = new Map<number, string | Map<number, string>>([[1, 'a'], [
       2,
-      new Map([[3, "c"]]),
+      new Map([[3, 'c']]),
     ]]);
-    const m3 = new Map<number, string | Map<number, string>>([[1, "a"], [
+    const m3 = new Map<number, string | Map<number, string>>([[1, 'a'], [
       2,
-      new Map([[3, "d"]]),
+      new Map([[3, 'd']]),
     ]]);
 
     eq(
@@ -311,7 +311,7 @@ describe("equals", () => {
     );
   });
 
-  it("should dispatch to `equals` method recursively in Map", () => {
+  it('should dispatch to `equals` method recursively in Map', () => {
     const a = new Map();
     const b = new Map();
     a.set(a, a);
@@ -322,7 +322,7 @@ describe("equals", () => {
     eq(equals(a, b), true);
   });
 
-  it("should compare Set objects by value", () => {
+  it('should compare Set objects by value', () => {
     eq(equals(new Set([]), new Set([])), true);
     eq(equals(new Set([]), new Set([1])), false);
     eq(equals(new Set([1]), new Set([])), false);
@@ -369,7 +369,7 @@ describe("equals", () => {
     );
   });
 
-  it("should dispatch to `equals` method recursively in Set", () => {
+  it('should dispatch to `equals` method recursively in Set', () => {
     const a = new Set();
     const b = new Set();
     a.add(a);
@@ -380,19 +380,19 @@ describe("equals", () => {
     eq(equals(a, b), true);
   });
 
-  it("should compare WeakMap objects by identity", () => {
+  it('should compare WeakMap objects by identity', () => {
     const m = new WeakMap([]);
     eq(equals(m, m), true);
     eq(equals(m, new WeakMap([])), false);
   });
 
-  it("should compare WeakSet objects by identity", () => {
+  it('should compare WeakSet objects by identity', () => {
     const s = new WeakSet([]);
     eq(equals(s, s), true);
     eq(equals(s, new WeakSet([])), false);
   });
 
-  it("should dispatch to `equals` method recursively", () => {
+  it('should dispatch to `equals` method recursively', () => {
     class Left {
       private value: any;
       constructor(x: any) {
@@ -428,7 +428,7 @@ describe("equals", () => {
     eq(equals([new Right(42)], [new Right(42)]), true);
   });
 
-  it("should commutative", () => {
+  it('should commutative', () => {
     class Point {
       protected x: number;
       protected y: number;
@@ -463,7 +463,7 @@ describe("equals", () => {
       }
     }
 
-    eq(equals(new Point(2, 2), new ColorPoint(2, 2, "red")), false);
-    eq(equals(new ColorPoint(2, 2, "red"), new Point(2, 2)), false);
+    eq(equals(new Point(2, 2), new ColorPoint(2, 2, 'red')), false);
+    eq(equals(new ColorPoint(2, 2, 'red'), new Point(2, 2)), false);
   });
 });
--- specs/findLastIndex.test.ts
+++ specs/findLastIndex.test.ts
@@ -1,15 +1,15 @@
-import { describe, it } from "./_describe.ts";
-import { _, findLastIndex, pipe, transduce } from "../mod.ts";
-import { eq } from "./utils/utils.ts";
+import { describe, it } from './_describe.ts';
+import { _, findLastIndex, pipe, transduce } from '../mod.ts';
+import { eq } from './utils/utils.ts';
 
-describe("findLastIndex", () => {
+describe('findLastIndex', () => {
   const obj1 = { x: 100 };
   const obj2 = { x: 200 };
   const a = [
     11,
     10,
     9,
-    "cow",
+    'cow',
     obj1,
     8,
     7,
@@ -23,49 +23,49 @@ describe("findLastIndex", () => {
     1,
     0,
   ];
-  const even = (x: any) => typeof x === "number" && x % 2 === 0;
-  const gt100 = (x: any) => typeof x === "number" && x > 100;
-  const isStr = (x: any) => typeof x === "string";
+  const even = (x: any) => typeof x === 'number' && x % 2 === 0;
+  const gt100 = (x: any) => typeof x === 'number' && x > 100;
+  const isStr = (x: any) => typeof x === 'string';
   const xGt100 = (o: any) => o && o.x > 100;
   const intoArray = (a: any) => [a];
 
-  it("should return the index of the last element that satisfies the predicate", () => {
+  it('should return the index of the last element that satisfies the predicate', () => {
     eq(findLastIndex(even, a), 15);
     eq(findLastIndex(gt100, a), 9);
     eq(findLastIndex(isStr, a), 3);
     eq(findLastIndex(xGt100, a), 10);
   });
 
-  it("should return -1 when no element satisfies the predicate", () => {
-    eq(findLastIndex(even, ["zing"]), -1);
+  it('should return -1 when no element satisfies the predicate', () => {
+    eq(findLastIndex(even, ['zing']), -1);
   });
 
-  it("returns the index of the last element into an array that satisfies the predicate", () => {
+  it('returns the index of the last element into an array that satisfies the predicate', () => {
     eq(intoArray(findLastIndex(even, a)), [15]);
     eq(intoArray(findLastIndex(gt100, a)), [9]);
     eq(intoArray(findLastIndex(isStr, a)), [3]);
     eq(intoArray(findLastIndex(xGt100, a)), [10]);
   });
 
-  it("returns -1 into an array when no element satisfies the predicate", () => {
-    eq(intoArray(findLastIndex(even, ["zing"])), [-1]);
+  it('returns -1 into an array when no element satisfies the predicate', () => {
+    eq(intoArray(findLastIndex(even, ['zing'])), [-1]);
   });
 
-  it("should works when the first element matches", () => {
+  it('should works when the first element matches', () => {
     eq(findLastIndex(even, [2, 3, 5]), 0);
   });
 
-  it("should not go into an infinite loop on an empty array", () => {
+  it('should not go into an infinite loop on an empty array', () => {
     eq(findLastIndex(even, []), -1);
   });
 
-  it("should return the curried versions too", () => {
+  it('should return the curried versions too', () => {
     eq(findLastIndex(even)(a), 15);
     eq(findLastIndex(_, a)(gt100), 9);
     eq(findLastIndex(isStr)(a), 3);
   });
 
-  it("should act as transducer", () => {
+  it('should act as transducer', () => {
     const t1 = pipe(findLastIndex(even));
     eq(t1(a), 15);
     eq(
